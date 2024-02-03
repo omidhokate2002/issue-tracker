3fc285158f2b02ec23cba7887bfaafecac660968
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-02-03 23:34:47 +0530

    Add loading skeleton

--- app/issues/loading.tsx
+++ app/issues/loading.tsx
@@ -1,15 +1,12 @@
+import { Table } from "@radix-ui/themes";
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
-import Link from "next/link";
-import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
 import IssueActions from "./new/IssueActions";
 
-const IssuesPage = async () => {
-  const issue = await prisma.issue.findMany();
-  await delay(2000);
-
+const LoadingIssuesPage = () => {
+  const issue = [1, 2, 3, 4, 5];
   return (
     <div>
       <IssueActions />
@@ -27,18 +24,18 @@ const IssuesPage = async () => {
         </Table.Header>
         <Table.Body>
           {issue.map((issue) => (
-            <Table.Row key={issue.id}>
+            <Table.Row key={issue}>
               <Table.Cell>
-                {issue.title}
+                <Skeleton />
                 <div className="block md:hidden">
-                  <IssueStatusBadge status={issue.status} />
+                  <Skeleton />
                 </div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                <IssueStatusBadge status={issue.status} />
+                <Skeleton />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.createdAt.toDateString()}
+                <Skeleton />
               </Table.Cell>
             </Table.Row>
           ))}
@@ -48,4 +45,4 @@ const IssuesPage = async () => {
   );
 };
 
-export default IssuesPage;
+export default LoadingIssuesPage;
