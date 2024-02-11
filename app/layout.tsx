e56449034a33d1ef1d3f2c38c72c47ea28dde545
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-02-11 22:37:50 +0530

    Fix avatar not loading

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,10 @@
-import { Container, Theme } from "@radix-ui/themes";
 import "@radix-ui/themes/styles.css";
+import "./theme-config.css";
+import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
-import "./globals.css";
-import "./theme-config.css";
 import AuthProvider from "./auth/Provider";
 
 const inter = Inter({
@@ -26,7 +26,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.variable}>
         <AuthProvider>
-          <Theme appearance="light" accentColor="violet">
+          <Theme accentColor="violet">
             <NavBar />
             <main className="p-5">
               <Container>{children}</Container>
