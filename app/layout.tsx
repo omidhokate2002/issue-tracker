0d5b7383f1c4d946c730207c6ba60a1c0af6e30b
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-02-12 21:48:06 +0530

    Revert "Set up React Query"
    This reverts commit 61b9ab48600adce3123f2d0d86839d5313e433ab.

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import { Inter } from "next/font/google";
 import { Container, Theme } from "@radix-ui/themes";
 import NavBar from "./NavBar";
 import AuthProvider from "./auth/Provider";
-import QueryClientProvider from "./QueryClientProvider";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,16 +25,14 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.variable}>
-        <QueryClientProvider>
-          <AuthProvider>
-            <Theme accentColor="violet">
-              <NavBar />
-              <main className="p-5">
-                <Container>{children}</Container>
-              </main>
-            </Theme>
-          </AuthProvider>
-        </QueryClientProvider>
+        <AuthProvider>
+          <Theme accentColor="violet">
+            <NavBar />
+            <main className="p-5">
+              <Container>{children}</Container>
+            </main>
+          </Theme>
+        </AuthProvider>
       </body>
     </html>
   );
