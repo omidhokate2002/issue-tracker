8620d09baf2fcc0d96d034d79ce3cbec11a664a3
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-03-07 23:52:27 +0530

    Build the pagination of the component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Pagination from "./components/Pagination";
+
 export default function Home() {
-  return <div>Hello World </div>;
+  return <Pagination itemCount={100} pageSize={10} currentPage={1} />;
 }
