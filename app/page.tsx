85d22b1d934f62c5f0d784dfd6e3083635a12b93
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-03-08 14:23:06 +0530

    Build the latest issue components

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,9 @@
-import Pagination from "./components/Pagination";
+import LatestIssue from "./LatestIssue";
 
-export default function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
+export default function Home() {
   return (
-    <Pagination
-      itemCount={100}
-      pageSize={10}
-      currentPage={parseInt(searchParams.page)}
-    />
+    <div>
+      <LatestIssue />
+    </div>
   );
 }
