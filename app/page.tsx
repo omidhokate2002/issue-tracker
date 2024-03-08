f9eb5da9259f09808c7a5fffa0d12c75ba710e97
OMi DHOKATE <42346279+omidhokate2002@users.noreply.github.com>
2024-03-09 00:05:21 +0530

    Build the issueSummarry component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import { open } from "fs";
+import IssueSummary from "./IssueSummary";
 import LatestIssue from "./LatestIssue";
 
-export default function Home() {
+export default async function Home() {
+  const open = await prisma?.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma?.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const in_Progress = await prisma?.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   return (
     <div>
-      <LatestIssue />
+      <IssueSummary open={open} inProgress={in_Progress} closed={closed} />
     </div>
   );
 }
